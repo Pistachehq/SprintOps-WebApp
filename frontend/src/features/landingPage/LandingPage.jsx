008ac3b77fd32e3a8c5f405cb7af9747dab56973
Maximo Rmz <maximo.guillermo.rmz@gmail.com>
2026-06-04 10:19:46 -0600

    actualizar el archivo

--- frontend/src/features/landingPage/LandingPage.jsx
+++ frontend/src/features/landingPage/LandingPage.jsx
@@ -1,5 +1,23 @@
+import PublicNavbar from "../../components/public_components/publicNavbar";
+import Layour from "../../components/Layout";
+import imageNav from "../../../public/ignore/image.png";
+import imageFoot from "../../../public/ignore/fut.png";
+import PublicFooter from "../../components/public_components/publicFooter";
+//<img src={imageNav} alt="Landing" className="w-full h-auto" />
+
 const LandingPage = () => {
-  return <div>Landing Page</div>;
+  return (
+    <div>
+      <header>
+        <PublicNavbar />
+        <img src={imageFoot} alt="Footer" className="w-full h-auto" />;
+      </header>
+      <main></main>
+      <footer>
+        <PublicFooter />
+      </footer>
+    </div>
+  );
 };
 
 export default LandingPage;
