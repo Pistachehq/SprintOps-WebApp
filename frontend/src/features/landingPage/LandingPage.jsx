3aa7ad1a4e4bd53299351ac2362f58ca4837dc11
Maximo Rmz <maximo.guillermo.rmz@gmail.com>
2026-06-04 11:20:02 -0600

    chore: elimina imports no utilizados

--- frontend/src/features/landingPage/LandingPage.jsx
+++ frontend/src/features/landingPage/LandingPage.jsx
@@ -1,16 +1,12 @@
 import PublicNavbar from "../../components/public_components/publicNavbar";
 import Layour from "../../components/Layout";
-import imageNav from "../../../public/ignore/image.png";
-import imageFoot from "../../../public/ignore/fut.png";
 import PublicFooter from "../../components/public_components/publicFooter";
-//<img src={imageNav} alt="Landing" className="w-full h-auto" />
 
 const LandingPage = () => {
   return (
     <div>
       <header>
         <PublicNavbar />
-        <img src={imageFoot} alt="Footer" className="w-full h-auto" />;
       </header>
       <main></main>
       <footer>
