a28063a1828672c26074b258130f7b4b0419dd2c
Maximo Rmz <maximo.guillermo.rmz@gmail.com>
2026-06-03 18:08:09 -0600

    refactor(components): move publicNavbar to public_components folder

--- frontend/src/components/publicNavbar.jsx
+++
@@ -1,5 +0,0 @@
-const PublicNavbar = () => {
-  return <div></div>;
-};
-
-export default PublicNavbar;
---
+++ frontend/src/components/public_components/publicNavbar.jsx
@@ -0,0 +1,28 @@
+import { useNavigate, Link, useLocation } from "react-router-dom";
+import Avatar from "../Avatar";
+import SearchBar from "../SearchBar";
+import FilledButton from "../../components/public_components/filled_button";
+import { useAuth } from "../../features/auth/hooks/useAuth";
+
+const PublicNavbar = () => {
+  return (
+    <nav className="h-[70px] bg-[#2B2B2B] px-10 flex items-center sticky top-0 z-40 shadow-sm justify-between">
+      <Link
+        to="/landingPage"
+        className="flex h-full shrink-0 items-center cursor-pointer py-1.5 pr-4"
+      >
+        <img
+          src="/logo-pistache.png"
+          alt="Pistache"
+          className="h-12 w-auto max-h-[58px] object-contain object-left"
+        />
+      </Link>
+
+      <div className="flex items-center gap-6 min-w-[200px] justify-end">
+        <FilledButton text="Iniciar Sesión" color="#4db6ac" path="/login" />
+      </div>
+    </nav>
+  );
+};
+
+export default PublicNavbar;
